abcc35113dd2c7258c925189f02b1e084a78a4b2
annshinewu <annshinewu@pacificamerican.org>
2017-05-18 13:30:26 +0800

    Update PAS database

--- PASNote/PASNote.sql
+++ PASNote/PASNote.sql
@@ -15,7 +15,7 @@ CREATE TABLE IF NOT EXISTS profile(
   profileID SERIAL PRIMARY KEY,
   userID INT,
   password VARCHAR(40),
-  birthday DATE,
+  birthday VARCHAR(40),
   contactEmail VARCHAR(40),
   activity VARCHAR(40),
   university VARCHAR(40),
@@ -38,13 +38,30 @@ INSERT INTO course VALUES (1, 'American Literature');
 INSERT INTO course VALUES (2, 'World Literature');
 INSERT INTO course VALUES (3, 'AP Statistics');
 INSERT INTO course VALUES (4, 'Physics');
+INSERT INTO course VALUES (5, 'SQL');
+INSERT INTO course VALUES (6, 'Python');
+INSERT INTO course VALUES (7, 'Pre-Calculus');
+INSERT INTO course VALUES (8, 'Calculus');
+INSERT INTO course VALUES (9, 'Chemistry');
+INSERT INTO course VALUES (10, 'Robotics');
+INSERT INTO course VALUES (11, 'Studyhall');
 
 CREATE TABLE IF NOT EXISTS courseList(
   courseID INT,
-  studentID INT
+  studentID INT,
+  FOREIGN KEY(courseID) REFERENCES course (courseID),
+  FOREIGN KEY(studentID) REFERENCES users(userID)
 );
 
 SELECT * FROM users;
+SELECT * FROM courseList;
+SELECT * FROM profile;
+
+INSERT INTO profile (userID, password, birthday, contactEmail, activity, university, major) VALUES (1, '1234','4/17/2002','a','a','a','a');
+INSERT INTO courseList (courseID, studentID) VALUES (1,1);
+INSERT INTO courseList (courseID, studentID) VALUES (2,1);
+INSERT INTO courseList (courseID, studentID) VALUES (3,1);
+
 
 CREATE OR REPLACE FUNCTION registerStudents(Variadic inputs VARCHAR []) RETURNS VOID AS
   $$
